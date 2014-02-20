c0403e97d809692598ede7bf3012ba92cda18610
David Simon <me@davidsimon.ch>
2014-02-20 01:01:36 +0100

    [Refactor] Started Refactoring AudioVis Scene

--- Assets/Scripts/PlayerCollector.cs
+++ Assets/Scripts/PlayerCollector.cs
@@ -2,25 +2,31 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerCollector : MonoBehaviour {
-	
-	public bool collectCoins = true;	
-	
-	void OnTriggerEnter(Collider collisionObj) {
+
+	public bool collectCoins = true;
+	private PlayerSettings _playerSettings;
+
+	public void Start () {
+		_playerSettings = this.GetComponent<PlayerSettings>();
+	}
+
+	public void OnTriggerEnter(Collider collisionObj) {
 		
 		if (collectCoins == true){
 			if( collisionObj.gameObject.tag == "Coin")
 			{
 				collisionObj.GetComponent<Coin>().Destruct();
-				this.gameObject.GetComponent<PlayerSettings>()._CoinCount++;
+				_playerSettings._CoinCount++;
 			}
-			else if (collisionObj.gameObject.tag == "AudioVisCoin")
-			{
-				collisionObj.GetComponent<AudioVisCoin>().Destruct();
-			}
-			else if (collisionObj.gameObject.tag == "AudioVisCoinChild")
-			{
-				collisionObj.transform.parent.GetComponent<AudioVisCoin>().Destruct();
-			}
+//			else if (collisionObj.gameObject.tag == "AudioVisCoin")
+//			{
+//				collisionObj.GetComponent<AudioVisCoin>().Destruct();
+//				_playerSettings._CoinCount++;
+//			}
+//			else if (collisionObj.gameObject.tag == "AudioVisCoinChild")
+//			{
+//				collisionObj.transform.parent.GetComponent<AudioVisCoin>().Destruct();
+//			}
 		}
 	}
 }
---
+++ Assets/Scripts/_AudioVis/AudioVisCollect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioVisCollect : MonoBehaviour {
+	
+	public bool collectCoins = true;
+	private PlayerSettings _playerSettings;
+	
+	public void Start () {
+		_playerSettings = this.GetComponent<PlayerSettings>();
+	}
+	
+	public void OnTriggerEnter(Collider collisionObj) {
+		
+		if (collectCoins == true){
+			if (collisionObj.gameObject.tag == "AudioVisCoin")
+			{
+				collisionObj.GetComponent<AudioVisCoin>().Destruct();
+				_playerSettings._CoinCount++;
+			}
+			else if (collisionObj.gameObject.tag == "AudioVisCoinChild")
+			{
+				collisionObj.transform.parent.GetComponent<AudioVisCoin>().Destruct();
+			}
+		}
+	}
+}
